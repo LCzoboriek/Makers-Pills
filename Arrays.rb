2a1a3942a950ddd8597edef06173813ec1f3f2e1
Luke Czoboriek <LC169782@protonmail.com>
2021-04-22 11:48:08 +0100

    added arrays pill knowledge

---
+++ Arrays.rb
@@ -0,0 +1,27 @@
+# An array in programming is an indexed collection of related items
+# Lets put some capitals of countries in an array
+capitals = ["London", "Paris", "Berlin"]
+
+# You can access any element in an array by specifying its index.
+
+capitals[2]
+# Will give you "Berlin"
+capitals[0]
+# Will give you "London"
+capitals[-1]
+# Will give you "Berlin"
+
+# You can also use first or last method on arrays
+capitals.first
+# Will give you "London"
+capitals.last
+# Will give you "Berlin"
+
+# Commonly user methods on arrays
+
+# pop
+# pop will remove the last element on an array and return the array if nothing is specified
+capitals.pop
+# Will give you ["London","Paris"]
+capitals.pop(2)
+# This will give you the last two elements in an array into a new array
